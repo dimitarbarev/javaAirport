4b272bb30f3b9cc55d0e3e497f1d1a52506ddab4
Dimitar Barev <501631@student.fontys.nl>
2024-05-13 11:04:13 +0200

    Initial commit

---
+++ src/main/java/com/example/javaairport/JavaAirportApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaairport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAirportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAirportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaairport/JavaAirportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaairport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAirportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
